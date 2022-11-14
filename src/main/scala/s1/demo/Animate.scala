bbe6bcbacdf765b652b8da16f7cc9052702d2cfc
Seuri Juuso <juuso.seuri@aalto.fi>
2022-11-14 10:50:33 +0200

    Improve some comments

--- src/main/scala/s1/demo/Animate.scala
+++ src/main/scala/s1/demo/Animate.scala
@@ -15,14 +15,19 @@ import s1.demo.effects.*
  *
  * In buffer "demo" please initiate in order the effects used in your
  * demo ( aka combination of effects ). Effects are changed when the
- * current effect returns true from it's "next" method.
+ * current effect returns true from its "next" method.
+ * 
+ * Music can be added by adding the path of the .wav file wrapped in Option.
+ * If music is not required for the demo, you can simply give the demoMusic value None.
+ * Music is only added for the demo as a whole, not to individual effects.
+ * NOTE: Unfortunately, the current version doesn't support MP3 files. wav is recommended.
  *
  * It is also possible to change the delay for effects
  * -> change the speed how quickly things change. Feel free to
  * change this variable and play with it.
  *
  * The variable top declares the top variable needed for SimpleSwingApplication.
- * In order to build your own effects and demos you don't have touch this variable.
+ * In order to build your own effects and demos you don't have to touch this variable.
  */
 
 object Animate extends SimpleSwingApplication {
